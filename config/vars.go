3f597bf3556f229631aff6a1b3dbe1c968a7c0de
Ismael Taboada Rodero <ismael.jtr@gmail.com>
2023-08-07 20:36:36 +0200

    fix: release 1.0.4

--- config/vars.go
+++ config/vars.go
@@ -1,8 +1,43 @@
 package config
 
+import (
+	"runtime"
+
+	"github.com/carlmjohnson/versioninfo"
+)
+
 // Automatically set by the compiler or build script
 var (
-	Version string
-	Build   string
-	OS      string
+	Version   string
+	Build     string
+	BuildTime string
+	OS        string
 )
+
+func GetVersion() string {
+	if Version == "" {
+		return versioninfo.Version
+	}
+	return Version
+}
+
+func GetBuild() string {
+	if Build == "" {
+		return versioninfo.Revision
+	}
+	return Build
+}
+
+func GetBuildTime() string {
+	if BuildTime == "" {
+		return versioninfo.LastCommit.Local().Format("2006-01-02T15:04+00:00")
+	}
+	return BuildTime
+}
+
+func GetOS() string {
+	if OS == "" {
+		return runtime.GOOS
+	}
+	return OS
+}
